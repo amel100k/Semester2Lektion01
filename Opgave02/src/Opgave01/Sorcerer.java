b01d6a02ebbc04af1a6eb396d756bd56dab4f757
amel100k <eaa23ema@students.eaaa.dk>
2024-02-01 12:00:33 +0100

    Added a lot of classes and their relations!

--- Opgave02/src/Opgave01/Sorcerer.java
+++ Opgave02/src/Opgave01/Sorcerer.java
@@ -3,7 +3,7 @@ package Opgave01;
 import java.util.ArrayList;
 
 public class Sorcerer extends Spellcaster{
-    public Sorcerer(ArrayList<Spell> spells) {
-        super(spells);
+    public Sorcerer(String name, int level) {
+        super(name, level);
     }
 }
--- Opgave02/src/Opgave01/Wizard.java
+++ Opgave02/src/Opgave01/Wizard.java
@@ -3,7 +3,7 @@ package Opgave01;
 import java.util.ArrayList;
 
 public class Wizard extends Spellcaster{
-    public Wizard(ArrayList<Spell> spells) {
-        super(spells);
+    public Wizard(String name, int level) {
+        super(name, level);
     }
 }
